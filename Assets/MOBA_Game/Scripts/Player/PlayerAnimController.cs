b87927b04588ea9eaa42889eda43024d185da5fe
zhangwei <378294113@qq.com>
2017-12-21 22:50:46 +0800

    动画同步

---
+++ Assets/MOBA_Game/Scripts/Player/PlayerAnimController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimController : MonoBehaviour
+{
+    private PhotonView m_photonView = null;
+    private Animator m_anim = null;
+
+    private void Awake()
+    {
+        m_anim = GetComponent<Animator>();
+        m_photonView = GetComponent<PhotonView>();
+    }
+
+    public void DoMove()
+    {
+        PlayMoveAnim();
+
+        m_photonView.RPC("PlayMoveAnim", PhotonTargets.Others);
+    }
+
+    public void DoIdle()
+    {
+        PlayIdleAnim();
+
+        m_photonView.RPC("PlayIdleAnim", PhotonTargets.Others);
+    }
+
+    [PunRPC]
+    private void PlayMoveAnim()
+    {
+        Debug.Log("Play Move Anim");
+        m_anim.SetBool("IsMove", true);
+        m_anim.SetBool("IsIdle", false);
+    }
+
+    [PunRPC]
+    private void PlayIdleAnim()
+    {
+        Debug.Log("Play Idle Anim");
+        m_anim.SetBool("IsIdle", true);
+        m_anim.SetBool("IsMove", false);
+    }
+}
--- Assets/MOBA_Game/Scripts/Player/PlayerController.cs
+++ Assets/MOBA_Game/Scripts/Player/PlayerController.cs
@@ -5,27 +5,67 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
+    public enum State
+    {
+        Idle,
+        Move
+    }
+
+    private PlayerAnimController m_animController = null;
     private Rigidbody m_rgd = null;
 
     private float m_moveForce = 100f;
     private NavMeshAgent m_agent;
 
+    private State m_curState = State.Idle;
+
     private void Start ()
     {
+        m_animController = GetComponent<PlayerAnimController>();
+
         m_rgd = GetComponent<Rigidbody>();
         m_agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update ()
     {
-		
-	}
+        UpdateState();
+    }
+
+    private void UpdateState()
+    {
+        if (m_curState == State.Move)
+        {
+            if (/*m_agent.pathStatus == NavMeshPathStatus.PathComplete &&*/ 
+                m_agent.remainingDistance <= 0.05f)
+            {
+                SetState(State.Idle);
+            }
+        }
+    }
+
+    private void SetState(State state)
+    {
+        m_curState = state;
+
+        if (m_curState == State.Idle)
+        {
+            Debug.Log("Set state idle");
+            m_animController.DoIdle();
+        }
+        else if (m_curState == State.Move)
+        {
+            Debug.Log("Set state move");
+            m_animController.DoMove();
+        }
+    }
 
     public void Move(Vector3 pos)
     {
-        //转向  
+        SetState(State.Move);
+
         transform.LookAt(new Vector3(pos.x, transform.position.y, pos.z));
-        //设置寻路的目标点  
+
         m_agent.SetDestination(pos);
     }
 }
